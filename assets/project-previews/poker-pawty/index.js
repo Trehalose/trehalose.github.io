ddac7ce5d05631b121138b39e2900ea8f6752757
Rina the Bane <Trehalose@users.noreply.github.com>
2023-09-27 16:20:03 -0400

    Update index.js

--- assets/project-previews/poker-pawty/index.js
+++ assets/project-previews/poker-pawty/index.js
@@ -79,6 +79,7 @@ const dealHand = () => {
   return hand;
 };
 
+const HAND_SIZE = 5;
 const HAND_TYPES = {
   'UNNAMED': () => ({
     index: 0,
@@ -145,7 +146,7 @@ const isFullHouse = (hand = []) => {
 };
 const isFlush = (hand = []) => {
   const firstCardSuit = hand[0].suit;
-  return hand.map(card => card.suit).filter(suit => suit === firstCardSuit).length === 5;
+  return hand.map(card => card.suit).filter(suit => suit === firstCardSuit).length === HAND_SIZE;
 };
 const isStraight = (hand = [], isRoyal = false) => {
   // NOTE: Ace is 14, TODO: refactor if in the mood for it
@@ -236,9 +237,22 @@ const setPlayers = (opponentCount = 1, preferredName = 'You') => {
   return players;
 };
 
-const sortTieBreak = (handA, handB) => {
+const getHighestKindRank = (hand = [], desiredCount = 0) => {
+  const kindCounts = {};
+  hand.forEach(card => {
+    kindCounts[card.rank] = kindCounts[card.rank] ? kindCounts[card.rank] + 1 : 1;
+  });
+
+  // Returns the highest rank for a specified kind count there are.
+  return Object.keys(kindCounts).filter(key => {
+    return kindCounts[key] === desiredCount;
+  }).map(key => Number(key)).sort((kindA, kindB) => {
+    return kindA - kindB;
+  })[0];
+};
+const generalTieBreak = (handA, handB) => {
   let sortDirection = 0; // defaults as a tied score
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < HAND_SIZE; i++) {
     if (handA[i].rank !== handB[i].rank) {
       // See sorting format explanation in sortForRoundWinner() below
       sortDirection = handA[i].rank < handB[i].rank ? 1 : -1;
@@ -247,6 +261,35 @@ const sortTieBreak = (handA, handB) => {
   }
   return sortDirection;
 };
+const cutTheTie = (playerA, playerB) => {
+  // Both players share the same hand type, which is why they are here
+  const handType = playerA.handType;
+  let kindCountRequest = 0;
+  switch (handType) {
+    case HAND_TYPES['PAIR']().index:
+    case HAND_TYPES['TWO_PAIR']().index:
+      kindCountRequest = 2;
+      break;
+    case HAND_TYPES['FULL_HOUSE']().index:
+    case HAND_TYPES['THREE_KIND']().index:
+      kindCountRequest = 3;
+      break;
+    case HAND_TYPES['FOUR_KIND']().index:
+      kindCountRequest = 4;
+      break;
+  }
+  const playerARanker = kindCountRequest !== 0 ? getHighestKindRank(playerA.hand, kindCountRequest) : 0;
+  const playerBRanker = kindCountRequest !== 0 ? getHighestKindRank(playerB.hand, kindCountRequest) : 0;
+  if (playerARanker < playerBRanker) {
+    return 1;
+  } else if (playerARanker > playerBRanker) {
+    return -1;
+  } else {
+    return generalTieBreak(playerA.hand, playerB.hand);
+  }
+};
+
+// Main export
 const sortForRoundWinner = (playerA, playerB) => {
   if (playerA.handType < playerB.handType) {
     return 1; // Higher hand type score
@@ -254,7 +297,7 @@ const sortForRoundWinner = (playerA, playerB) => {
     return -1; // Lower hand type score
   } else {
     // Equivalent hand type score
-    return sortTieBreak(playerA.hand, playerB.hand);
+    return cutTheTie(playerA, playerB);
   }
 };
 
